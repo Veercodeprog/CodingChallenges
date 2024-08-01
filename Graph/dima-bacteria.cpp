1212b08cc5e77d26ec8935da3f78e364681ea268
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-08-01 21:48:29 +0530

    dima-bacteria-disjoint-set

--- Graph/dima-bacteria.cpp
+++ Graph/dima-bacteria.cpp
@@ -1,122 +1,146 @@
-#include <climits>
-#include <cstring>
+#include <algorithm>
 #include <iostream>
-#include <queue>
+#include <tuple>
 #include <vector>
+
 using namespace std;
+using ll = long long;
+constexpr int INF = 0x3f3f3f3f; // Represents infinity
 
-const int INF = INT_MAX;
-const int MAXN = 100005;            // Adjust based on problem constraints
-vector<pair<int, int>> graph[MAXN]; // adjacency list for the graph
-
-// pair<int, int>: A pair is a simple container from the C++ Standard Library
-// that holds two values.
-//
-void floydWarshall(int n, vector<vector<int>> &dist) {
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= n; ++j) {
-      dist[i][j] = INF;
-    }
-    dist[i][i] = 0;
+// Find the root of the set containing `x` using path compression
+int findRoot(vector<int> &parent, const int &x) {
+  if (parent[x] == x) {
+    return x;
+  } else {
+    int root = findRoot(parent, parent[x]);
+    parent[x] = root;
+    return root;
   }
+}
 
-  for (int u = 1; u <= n; ++u) {
-    for (auto [v, cost] : graph[u]) {
-      dist[u][v] = cost;
-      dist[v][u] = cost;
-    }
-    for (int k = 1; k <= n; ++k) {
-      for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-          if (dist[i][k] < INF && dist[k][j] < INF) {
-            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-          }
+// Merge the sets containing `x` and `y`
+void unionSets(vector<int> &parent, const int &x, const int &y) {
+  int rootX = findRoot(parent, x);
+  int rootY = findRoot(parent, y);
+  if (rootX != rootY) {
+    parent[rootX] = rootY;
+  }
+}
+// Assign the same type to all bacteria in the range [left, right]
+void assignType(vector<int> &typeAssignment, const int &left, const int &right,
+                const int &type) {
+  for (int i = left; i <= right; i++) {
+    typeAssignment[i] = type;
+  }
+}
+// Apply Floyd-Warshall algorithm to compute all-pairs shortest paths
+void computeShortestPaths(vector<vector<int>> &distanceMatrix,
+                          const int &size) {
+  for (int k = 0; k < size; k++) {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (distanceMatrix[i][k] < INF && distanceMatrix[k][j] < INF) {
+          distanceMatrix[i][j] =
+              min(distanceMatrix[i][j],
+                  distanceMatrix[i][k] + distanceMatrix[k][j]);
         }
+      }
+    }
+  }
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (distanceMatrix[i][j] == INF) {
+        distanceMatrix[i][j] = -1;
       }
     }
   }
 }
+bool areAllSameType(vector<int> &typeRoot, const int &left, const int &right) {
+  int rootType = typeRoot[right];
+  for (int i = left; i < right; i++) {
+    if (typeRoot[i] != rootType) {
+      return false;
+    }
+  }
+  return true;
+}
+tuple<bool, vector<vector<int>>>
+process(int numBacteria, int numConnections, int numTypes,
+        const vector<int> &typeCounts,
+        const vector<tuple<int, int, int>> &connections) {
+
+  vector<int> parent(numBacteria + 1);
+  for (int i = 1; i <= numBacteria; i++) {
+    parent[i] = i;
+  }
+
+  for (const auto &[u, v, cost] : connections) {
+    if (cost == 0) {
+
+      unionSets(parent, u, v);
+    }
+  }
+
+  vector<int> typeRoot(numBacteria + 1);
+  for (int i = 1; i <= numBacteria; i++) {
+    typeRoot[i] = findRoot(parent, i);
+  }
+  vector<int> typeAssignment(
+      numBacteria +
+      1); // for each bacteria, the type of the root of the set it belongs to
+  int left = 1, right = 0;
+  for (int type = 0; type < numTypes; type++) {
+    right += typeCounts[type];
+    bool allSame = areAllSameType(typeRoot, left, right);
+    if (!allSame) {
+      return {false, vector<vector<int>>(numTypes, vector<int>(numTypes, 0))};
+    }
+    assignType(typeAssignment, left, right, type);
+    left = right + 1;
+  }
+  vector<vector<int>> distanceMatrix(numTypes, vector<int>(numTypes, INF));
+  for (int type = 0; type < numTypes; type++) {
+    distanceMatrix[type][type] = 0;
+  }
+  for (const auto &[u, v, cost] : connections) {
+    int typeU = typeAssignment[u];
+    int typeV = typeAssignment[v];
+    distanceMatrix[typeU][typeV] = min(distanceMatrix[typeU][typeV], cost);
+    distanceMatrix[typeV][typeU] = min(distanceMatrix[typeV][typeU], cost);
+  }
+  computeShortestPaths(distanceMatrix, numTypes);
+  return {true, distanceMatrix};
+}
 
 int main() {
-  int n, m, k;
-  cin >> n >> m >> k;
-  vector<int> types(n + 1); // vactor with n+1 element as we waant to use 1
-                            // based indexing for the vectory of
-  vector<vector<int>> bacteria_of_type(k + 1);
-  // bacteria_of_type(k + 1): This creates a vector with k + 1 elements, where
-  // each element is an empty vector<int>.
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-  // vector<vector<pair<int, int>>> adj(n + 1); // adjacency list
-  // it is vector of vector of pair as we want to store the weight of the edge
-  // as well its strructture is adj[1] = { {2, 3}, {3, 4} } means there is an
-  // edge from 1 to 2 with weight 3 and from 1 to 3 with weight
-
-  // for number of bacteria of each typei
-  int totalCount;
-
-  for (int i = 1; i <= k; i++) {
-
-    int count;
-    if (totalCount > n) {
-      cout << "bacterias exceed the total number of bacteria i.e n" << endl;
-      return 0;
-    }
-
-    cin >> count;
-    totalCount += count;
-    for (int j = 0; j < count; ++j) {
-      int bact;
-      cin >> bact;
-      bacteria_of_type[i].push_back(bact);
-      types[bact] = i; // Assign type to bacteria
-    }
+  int numBacteria = 0, numConnections = 0, numTypes = 0;
+  cin >> numBacteria >> numConnections >> numTypes;
+  vector<int> typeCounts(numTypes, 0);
+  for (int i = 0; i < numTypes; i++) {
+    cin >> typeCounts[i];
   }
-
-  for (int i = 0; i < m; i++) {
-
-    int u, v, w;
-    cin >> u >> v >> w;
-    graph[u].emplace_back(v, w);
-    graph[v].emplace_back(u, w); // emplace_back is a function that adds an
-                                 //  element to the end of a vector
+  vector<tuple<int, int, int>> connections;
+  for (int i = 0; i < numConnections; i++) {
+    int u = 0, v = 0, cost = 0;
+    cin >> u >> v >> cost;
+    connections.emplace_back(u, v, cost);
   }
-  vector<vector<int>> dist(
-      n + 1, vector<int>(n + 1)); // vector of type vector with n+1 rows and
-                                  // each row has vector of n+1 elements
-  floydWarshall(n, dist);
-  vector<vector<int>> d(k + 1, vector<int>(k + 1, -1));
-  bool has_zero_cost_pair = false;
-
-  // Fill the k x k matrix d based on the distance matrix
-  for (int i = 1; i <= k; ++i) {
-    for (int j = 1; j <= k; ++j) {
-      bool found_zero = false;
-      for (int u : bacteria_of_type[i]) {
-        for (int v : bacteria_of_type[j]) {
-          if (dist[u][v] == 0) {
-            found_zero = true;
-            break;
-          }
-        }
-        if (found_zero)
-          break;
-      }
-      d[i][j] = found_zero ? 0 : -1;
-      if (found_zero) {
-        has_zero_cost_pair = true;
-      }
-    }
-  }
-
-  if (has_zero_cost_pair) {
+  auto [isPossible, resultMatrix] =
+      process(numBacteria, numConnections, numTypes, typeCounts, connections);
+  if (isPossible) {
     cout << "Yes\n";
-    for (int i = 1; i <= k; ++i) {
-      for (int j = 1; j <= k; ++j) {
-        cout << d[i][j] << " ";
+    for (int i = 0; i < numTypes; i++) {
+      for (int j = 0; j < numTypes; j++) {
+        cout << resultMatrix[i][j] << " ";
       }
       cout << "\n";
     }
   } else {
     cout << "No\n";
   }
+
+  return 0;
 }
