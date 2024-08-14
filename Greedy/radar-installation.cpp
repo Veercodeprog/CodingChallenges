712e9869a1cb82e5d0e0628ce5b55f17d56ed4ec
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-08-14 15:11:06 +0530

    radar installation

--- Greedy/radar-installation.cpp
+++ Greedy/radar-installation.cpp
@@ -9,34 +9,33 @@ int numOfIslands, currentIndex;
 // Array to store the x-axis coverage range of each island by radar
 pair<double, double> coverageRange[1005];
 int calculateRadarCount() {
-  double currentCoverageEnd = -1e9;
+  double range = -1e9;
   int radarCount = 0;
   sort(coverageRange, coverageRange + numOfIslands);
   currentIndex = 0;
   while (currentIndex < numOfIslands) {
 
     while (currentIndex < numOfIslands &&
-           coverageRange[currentIndex].first <= currentCoverageEnd) {
-      currentCoverageEnd =
-          min(currentCoverageEnd, coverageRange[currentIndex].second);
+           coverageRange[currentIndex].first <= range) {
+      range = min(range, coverageRange[currentIndex].second);
       ++currentIndex;
     }
     ++radarCount;
-    if (currentIndex < numberOfIslands) {
-      currentCoverageEnd = coverageRange[currentIndex].second;
-    }
+    range = coverageRange[currentIndex].second;
+    currentIndex++;
   }
+  cout << "coverage range pair: " << coverageRange << endl;
+  return radarCount - 1;
 }
 
 int main() {
 
   double x, y, d;
-  cin >> numOfIslands >> d;
   int invalidCase, caseNum = 0;
 
-  while (cin >> numOfIslands >> d && numOfIslands) {
+  while ((cin >> numOfIslands >> d) && numOfIslands) {
     invalidCase = 0;
-    for (int i = 0; i < numOfIslands; i++) {
+    for (int i = 0; i < numOfIslands; ++i) {
       cin >> x >> y;
       if (y > d) {
         invalidCase = 1;
@@ -44,8 +43,9 @@ int main() {
         coverageRange[i].first = x - sqrt(d * d - y * y);
         coverageRange[i].second = x + sqrt(d * d - y * y);
       }
-      cout << "Case " << ++currentIndex << ": "
-           << (invalidCase ? -1 : calculateRadarCount()) << endl;
     }
+    cout << "Case " << ++caseNum << ": "
+         << (invalidCase ? -1 : calculateRadarCount()) << endl;
   }
+  return 0;
 }
